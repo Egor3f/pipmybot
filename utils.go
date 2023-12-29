4a11d3e29230a074f66725fbcba9e87340d5563a
Egor Aristov <ef@efprojects.com>
2023-12-30 00:09:23 +0300

    fix

--- utils.go
+++ utils.go
@@ -5,7 +5,7 @@ import "strings"
 func stringContains(source string, check []string) bool {
 	source = strings.ToLower(source)
 	for _, st := range check {
-		if strings.Contains(source, st) {
+		if strings.HasPrefix(source, st) {
 			return true
 		}
 	}
